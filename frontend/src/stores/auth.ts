7a8377df1015f703c49b7fa0320b46e58e2677c5
UniTaeMin <onnu0802@naver.com>
2020-02-19 17:11:36 +0900

    Add Auth Store signUp()
    Add Auth Store signUp()

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -1,11 +1,11 @@
 import { observable, action } from "mobx"
 import { createContext } from "react"
+import Axios from "../libs/axios"
 class authStore {
   @observable count: number = 2
 
-  @action countUp = (): number => {
-    this.count = 9
-    return this.count
+  @action signUp = async (id: string, pwd: string, pwd2: string, username: string, email: string, profile: object, terms: boolean) => {
+    await Axios.Post("/", { id, pwd, pwd2, username, email, profile, terms })
   }
 }
 
