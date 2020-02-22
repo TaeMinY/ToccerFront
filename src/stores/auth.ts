0ce1b097131f5244e83a08555b61a252dd28665f
UniTaeMin <onnu0802@naver.com>
2020-02-23 03:34:50 +0900

    Add store auth token
    Add store auth token

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -38,6 +38,17 @@ class authStore {
       })
     return turn
   }
+  @action token: any = async (token: any) => {
+    var turn: any
+    await Axios.Post("/account/token", { token: token })
+      .then((result: any) => {
+        turn = result
+      })
+      .catch((err: any) => {
+        console.log(err)
+      })
+    return turn
+  }
 }
 //아이디 중복 해결하기
 export const authStoreContext = createContext(new authStore())
