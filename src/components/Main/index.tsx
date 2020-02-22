94e6fdbc83466b72f14a9e42c7b1d10df0ac2fad
UniTaeMin <onnu0802@naver.com>
2020-02-22 10:14:21 +0900

    Update Swiper
    Update Swiper

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -21,11 +21,21 @@ function Main() {
     <Wrap>
       <div>
         <Swiper {...params}>
-          <img src={require("../../assets/swiper__one.png")} alt="swiper1" width="100%" height="100%" style={{ objectFit: "cover" }} />
-          <img src={require("../../assets/swiper__one.png")} alt="swiper1" width="100%" height="100%" style={{ objectFit: "cover" }} />
-          <img src={require("../../assets/swiper__one.png")} alt="swiper1" width="100%" height="100%" style={{ objectFit: "cover" }} />
-          <img src={require("../../assets/swiper__one.png")} alt="swiper1" width="100%" height="100%" style={{ objectFit: "cover" }} />
-          <img src={require("../../assets/swiper__one.png")} alt="swiper1" width="100%" height="100%" style={{ objectFit: "cover" }} />
+          <SwiperItem>
+            <img src={require("../../assets/swiper__one.png")} alt="swiper1" width="100%" height="100%" style={{ objectFit: "cover" }} />
+            <SwiperText>토트넘</SwiperText>
+            <SwiperShadow></SwiperShadow>
+          </SwiperItem>
+          <SwiperItem>
+            <img src={require("../../assets/swiper__one.png")} alt="swiper1" width="100%" height="100%" style={{ objectFit: "cover" }} />
+            <SwiperText>토트넘</SwiperText>
+            <SwiperShadow></SwiperShadow>
+          </SwiperItem>
+          <SwiperItem>
+            <img src={require("../../assets/swiper__one.png")} alt="swiper1" width="100%" height="100%" style={{ objectFit: "cover" }} />
+            <SwiperText>토트넘</SwiperText>
+            <SwiperShadow></SwiperShadow>
+          </SwiperItem>
         </Swiper>
         <div>dd</div>
         <div>dd</div>
@@ -53,4 +63,27 @@ const Wrap = styled.div`
       height: 500px;
     }
   }
+`
+const SwiperItem = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+`
+const SwiperShadow = styled.div`
+  background-color: rgba(0, 0, 0, 0.25);
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
+const SwiperText = styled.div`
+  position: absolute;
+  bottom: 10%;
+  left: 0;
+  right: 0;
+  font-size: 40px;
+  z-index: 100;
+  text-align: center;
+  color: white;
 `
