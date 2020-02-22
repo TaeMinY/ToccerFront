9a265b52d2b189f39e12ab83d6f639cd64b8e3f6
UniTaeMin <onnu0802@naver.com>
2020-02-23 01:02:34 +0900

    Change filename Notice -> Board
    Change filename Notice -> Board

---
+++ src/components/Main/Board.tsx
@@ -0,0 +1,127 @@
+import React, { useState } from "react"
+import styled from "styled-components"
+function Text() {
+  return (
+    <>
+      <Notice>
+        <NoticeBox>공지</NoticeBox>
+        <NoticeText>토트넘 PL 19 ~ 20 우승 실패 확정</NoticeText>
+      </Notice>
+      <BoradText>
+        <img src={require("../../assets/ico.gif")} alt="점" width="3px" height="3px;" style={{ backgroundColor: "black", marginRight: "10px" }} />
+        <BoradTitle>게시판테스트</BoradTitle>
+      </BoradText>
+    </>
+  )
+}
+function Board() {
+  return (
+    <Wrap>
+      <TitleWrap>
+        <Title>게시판</Title>
+        <TitleView>더보기</TitleView>
+      </TitleWrap>
+      <TitleLine>
+        <TextWrap>
+          <Text></Text>
+        </TextWrap>
+      </TitleLine>
+    </Wrap>
+  )
+}
+
+export default Board
+
+const Wrap = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  min-width: 980px;
+  height: 370px;
+  flex-direction: column;
+  @media only screen and (max-width: 1445px) {
+    min-width: 100%;
+  }
+`
+const Title = styled.div`
+  font-size: 20px;
+  font-family: "NanumSRB";
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const TitleLine = styled.div`
+  width: 100%;
+  height: 100%;
+  border-top: 1.5px solid black;
+  margin-top: 5px;
+  display: flex;
+`
+const TitleWrap = styled.div`
+  display: flex;
+  min-height: 28px;
+  justify-content: space-between;
+`
+const TitleView = styled.div`
+  font-size: 12px;
+  border-radius: 5px;
+  border: 1px solid #0f204b;
+  width: 62px;
+  height: 27px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 9.5px;
+  cursor: pointer;
+  transition: 0.2s;
+  &:hover {
+    background-color: #0f204b;
+    color: white;
+  }
+`
+const TextWrap = styled.div`
+  width: 100%;
+  height: 100%;
+`
+const Notice = styled.div`
+  width: 100%;
+  height: 36px;
+  padding: 5px 10px;
+  background-color: #f2f2f2;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  border-bottom: 0.5px solid #f4efff;
+`
+const NoticeBox = styled.div`
+  height: 22px;
+  width: 56px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #ffc6c9;
+  color: #ff4e59;
+  background-color: #ffe3e4;
+  font-weight: 800;
+  font-size: 11px;
+  border-radius: 3px;
+`
+const NoticeText = styled.div`
+  font-size: 13px;
+  color: #ff4e59;
+  font-weight: 800;
+  margin-left: 10px;
+`
+const BoradText = styled.div`
+  width: 100%;
+  height: 36px;
+  padding: 5px 10px;
+  background-color: white;
+  box-sizing: border-box;
+  font-size: 13px;
+  display: flex;
+  align-items: center;
+  border-bottom: 0.2px solid #f4efff;
+`
+const BoradTitle = styled.div``
