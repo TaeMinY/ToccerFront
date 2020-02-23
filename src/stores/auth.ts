22cac51af1c1edc3c925b5be118d67a109c1bcac
UniTaeMin <onnu0802@naver.com>
2020-02-23 19:00:27 +0900

    Add store action token
    Add store action token

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -3,6 +3,7 @@ import { createContext } from "react"
 import Axios from "../libs/axios"
 class authStore {
   @observable state: boolean = false
+  @observable tokenState: boolean = false
 
   @action signUp: any = async (id: string, pwd: string, pwd2: string, username: string, email: string, terms: boolean, profile__expansion: string) => {
     var turn: any
@@ -38,16 +39,19 @@ class authStore {
       })
     return turn
   }
-  @action token: any = async (token: any) => {
-    var turn: any
+  @action token = async (token: any) => {
     await Axios.Post("/account/token", { token: token })
       .then((result: any) => {
-        turn = result
+        if (result.data.state) {
+          console.log("성공")
+          this.tokenState = true
+        } else {
+          this.tokenState = false
+        }
       })
       .catch((err: any) => {
         console.log(err)
       })
-    return turn
   }
 }
 //아이디 중복 해결하기
